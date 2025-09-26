ffaf079c688e621211de5d2ad2577854fe791c85
Seu Novo Nome <seuemail@exemplo.com>
2025-09-26 16:32:23 -0300

    ddd

--- script.js
+++ script.js
@@ -93,17 +93,7 @@ const App = {
                     <div class="news-content">
                         <div class="card-meta">
                             <time class="news-date" datetime="${article.date}">${article.displayDate}</time>
-                            <a href="${article.instagramLink}" target="_blank"
-                                rel="noopener noreferrer" class="instagram-link"
-                                aria-label="Ver esta notícia no Instagram">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round">
-                                    <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-                                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                                </svg>
-                            </a>
+                            ${this.createInstagramLink(article.instagramLink)}
                         </div>
                         <a href="${article.link}"
                             target="_blank" rel="noopener noreferrer" class="stretched-link">
@@ -115,6 +105,25 @@ const App = {
                         </div>
                     </div>
                 </article>
+            `;
+        },
+
+        createInstagramLink(url) {
+            if (!url) {
+                return ''; // Retorna uma string vazia se a URL não existir
+            }
+            return `
+                <a href="${url}" target="_blank"
+                    rel="noopener noreferrer" class="instagram-link"
+                    aria-label="Ver esta notícia no Instagram">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                    </svg>
+                </a>
             `;
         }
     },
